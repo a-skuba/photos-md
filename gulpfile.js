51646fdcf05f44df01c977fc37a4a8530677c728
Anej Skubic <anej@skuba-buba.com>
2017-02-10 14:22:03 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Gulp build script
-// 
+//
 // [help] https://css-tricks.com/gulp-for-beginners/
 //        https://una.im/gulp-local-psi/
 // [autoprefix] https://github.com/postcss/autoprefixer#browsers
@@ -12,6 +12,8 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     ftp = require('gulp-ftp'),
     ftpData = require('./ftp.json'),
+    babel = require('gulp-babel'),
+    minify = require('gulp-minify'),
     sass = require('gulp-sass');
 
 // SASS
@@ -22,22 +24,44 @@ gulp.task('sass', function () {
             outputStyle: 'compressed',  // nested, expanded, compact, compressed
         }))
         .pipe(autoprefixer(['last 3 versions', '> 1% in SI']))
-        //.pipe(browserSync.stream())
-        .pipe(ftp(ftpData))
+        //.pipe(ftp(ftpData))
         .pipe(gulp.dest('dist'));
 });
 
+// JS
+gulp.task('js', function () {
+  return gulp.src('src/js/*.js')
+      .pipe(babel({
+          plugins: ['transform-es2015-modules-umd']
+    }))
+    .pipe(minify({
+        ext: {
+            src: '.js',
+            min:'.min.js'
+        },
+        //exclude: ['/dir'],
+        ignoreFiles: ['.min.js', 'xapp.js']
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
 // WATCH
-gulp.task('watch', ['sass'], function () {
+gulp.task('watch', ['server'], function () {
     gulp.watch('src/css/photos-md.scss', ['sass']);
+    gulp.watch('src/js/photos-md.js', ['js']);
 });
 
 // SERVE
-gulp.task('serve', function () {
-    browserSync.init({
-        proxy: "localhost",
-        /*server: {
-            baseDir: 'demo'
-        }*/
+gulp.task('server', function () {
+    return browserSync.init({
+        files: [
+            "dist/**/*.?(css|js|png|jpeg|jpg)",
+            "*.?(php|html)"
+        ],
+        proxy: "http://pmd.localhost/",
+        logFileChanges: true,
+        browser: ["chrome"],
+        injectChanges: true,
+        //startPath: "/demo"
     });
 });
